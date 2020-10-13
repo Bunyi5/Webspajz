d4bf252f0a4076612197d4cf3235401f71974918
Bunyi5 <bbakos262@gmail.com>
2020-10-13 14:26:53 +0200

    Configure project to Spring project

---
+++ src/main/java/com/thesis/webspajz/WebspajzApplication.java
@@ -0,0 +1,13 @@
+package com.thesis.webspajz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebspajzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebspajzApplication.class, args);
+	}
+
+}
